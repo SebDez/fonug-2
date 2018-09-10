f78d1e96618d73e16a9aa54a1b014bc7c7fdc0af
SebDez <sebastien.dez@ocea-smart-building.fr>
2018-09-10 13:59:21 +0200

    wip : designing home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="home">
-    HOME
-  </div>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">FONUG est un générateur aléatoire de personnage.</h3>
+          </div>
+        </v-card-title>
+
+        <v-card-text>
+          <p class="text-md-center">Il convient autant pour la génération rapide de Personages Non Joueurs (PNJ) que
+            pour d'autres personnages de fiction, le but n'est pas de générer une fiche personnage mais de bien donner des pistes lors de la création du personnage.
+          Vous pouvez préselectionner le genre de votre personnage si vous le désirez, puis lancez la génération.</p>
+        </v-card-text>
+
+        <v-select
+          :items="genders"
+          :value="genderSelected"
+          label="Select gender"
+          solo
+        ></v-select>
+
+        <v-card-actions>
+        <v-btn class="text-md-center" @click="generateCharacter" color="info">Generer mon personnage</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      genders: [
+        { text: 'Peu importe', value: 0 },
+        { text: 'Male', value: 1 },
+        { text: 'Femelle', value: 2 }
+      ],
+      genderSelected: 0
+    }
+  },
+  methods: {
+    generateCharacter () {
+      // call action for generating
+      this.$router.push({ name: 'character' })
+    }
+  }
 }
 </script>
