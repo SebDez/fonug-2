9562cbce59e5891826aff7212b4abdd43c15b8c0
Sébastien Dez <sdez@peaks.fr>
2018-12-13 16:57:41 +0100

    style: rework home style

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const GENDERS = [
+  { text: 'Peu importe', value: 0 },
+  { text: 'Masculin', value: 1 },
+  { text: 'Féminin', value: 2 }
+]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,52 @@
 <template>
-  <VLayout>
-    <VFlex xs12 sm6 offset-sm3>
-      <VCard>
+  <VLayout column text-xs-center>
+    <VCard>
+      <VFlex>
         <VCardTitle primary-title>
           <div>
             <h3 class="headline mb-0">
-              FONUG est un générateur aléatoire de personnage.
+              Nouveau personnage
             </h3>
           </div>
         </VCardTitle>
+      </VFlex>
 
-        <VCardText>
-          <p class="text-md-center">
-            Il convient autant pour la génération rapide de Personages Non Joueurs (PNJ) que
-            pour d'autres personnages de fiction, le but n'est pas de générer une fiche personnage mais de bien donner des pistes lors de la création du personnage.
-            Vous pouvez préselectionner le genre de votre personnage si vous le désirez, puis lancez la génération.
-          </p>
-        </VCardText>
+      <VFlex>
+        <p class="text-md-center">
+          Genre du personnage
+        </p>
+      </VFlex>
 
-        <VSelect
-          :items="genders"
-          :value="genderSelected"
-          label="Select gender"
-          solo
-        />
+      <VCardActions class="justify-center">
+        <VFlex xs8>
+          <VSelect
+            :items="GENDERS"
+            :value="genderSelected"
+            solo
+            outline
+          />
+        </VFlex>
+      </VCardActions>
 
-        <VCardActions>
-          <VBtn class="text-md-center" color="info" @click="generateCharacter">
-            Generer mon personnage
-          </VBtn>
-        </VCardActions>
-      </VCard>
-    </VFlex>
+      <VFlex>
+        <VBtn color="pink"
+              @click="generateCharacter"
+        >
+          Generer mon personnage
+        </VBtn>
+      </VFlex>
+    </VCard>
   </VLayout>
 </template>
 
 <script>
+import { GENDERS } from '@/constants'
+
 export default {
   name: 'home',
   data () {
     return {
-      genders: [
-        { text: 'Peu importe', value: 0 },
-        { text: 'Male', value: 1 },
-        { text: 'Femelle', value: 2 }
-      ],
+      GENDERS,
       genderSelected: 0
     }
   },
